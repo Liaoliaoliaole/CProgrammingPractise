88297318d1897f59ed2127a21accca658e8376c7
Liyuan Liu <tenderlili@gmail.com>
2022-03-24 08:21:37 +0200

    set colors in linux compiler,port back to windows.

--- Project1/pasMath.h
+++ Project1/pasMath.h
@@ -1,10 +1,19 @@
 #pragma once
 // a header file in c/c++ usually contains 3 parts: 1. Constant definition 2. function prototype  3. structure definition
 //1. Constant definition 
-#define MAX_LINE 10
+# define WINDOWS //if you want port back just add //
+#define MAX_LINE 12
+enum COLORS { BLACK = 30, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE };
+//to define multiple values:must be continuosly number, only the first need to be writen.
 
 //2.  function prototype
 int binom(int a, int b);
 int fact(int a);
+
+void resetColors(void);
+void setForeground(int);
+void setBackground(int);//declareation doesn't need argument name
+void clearScreen();
+void gotoXY(int, int);
 
 
--- Project1/pascal.c
+++ Project1/pascal.c
@@ -1,23 +1,50 @@
 //this project displays a Pascal triangle up to N rows
 #include <stdio.h>
 #include"pasMath.h"
+#ifdef WINDOWS
+#include<windows.h>
+#endif
 
 
 int main() {
     int x, n, k;
+
+#ifdef WINDOWS
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD mode = 0;
+    GetConsoleMode(h, &mode);
+    mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(h, mode);
+#endif
+    clearScreen();
+    //setup background and font color for a title pascal triangle
+    setForeground(YELLOW);
+    setBackground(BLUE);
+    gotoXY(1, 20);
+    printf("Pascal TRiangle");
+    resetColors();
+    printf("\n");
+    char str[100];
     for (n = 0; n < MAX_LINE; n++) {
         for (x = MAX_LINE; x > n; x--)
         {
             printf("  ");
-        }//print spacees to shape thetriangle centralized
-
+        }
+        //set each line with different color. 
+        setForeground(RED + n % 7);
         for (k = 0; k <= n; k++) {
             printf("%-4d ", binom(n, k));//output always 4 spaces
         }
         printf("\n");
     }
+    resetColors();
     return 0;
 }
+
+
+
+
+
 
 
 
